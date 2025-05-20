a1303cbac70fb49238deb6d4dd03e0e14b76ae89
Samuesl <ruess.samuel@gmail.com>
2025-05-20 16:48:33 +0200

    windpfeile

--- main.js
+++ main.js
@@ -107,3 +107,26 @@ map.fire("click", {
         lng: ibk.lng,
     }
 })
+
+// Windpfeile
+async function loadWind(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    console.log(jsondata);
+
+    let velocityLayer = L.velocityLayer({
+        displayValues: true,
+        lineWidth: 2,
+        displayOptions: {
+        
+        velocityType: "",
+          position: "bottomright",
+          emptyString: "Keine Daten vorhanden",
+          speedUnit: "k/h",
+          directionString: "Windrichtung",
+          speedString: "Windgeschwindigkeit",
+        },
+        data: jsondata,
+      }).addTo(overlays.wind_richtung);
+}
+loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json");
