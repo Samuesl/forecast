8e6a2f81d2363615f7a4e9a773face81332362b1
Samuesl <ruess.samuel@gmail.com>
2025-05-22 16:13:22 +0200

    Vorhersage zeitpunkt auf der HTMl Seite anzeigen

--- main.js
+++ main.js
@@ -111,12 +111,16 @@ async function loadWind(url) {
     let response = await fetch(url);
     let jsondata = await response.json();
     //console.log(jsondata[0].header.refTime);
-    //console.log(jsondata[0].header.forecastTime);
+    c//onsole.log(jsondata[0].header.forecastTime);
     let forecastDate = new Date(jsondata[0].header.refTime);
     forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime);
     //console.log(forecastDate);
 
-
+    let forecastSpan = document.querySelector("#forecast-link");
+    //console.log(forecastSpan);
+    forecastSpan.innerHTML = `
+            <a href="${url}" target="met.no">${forecastDate.toLocaleString()}</a>)
+    `;
     L.velocityLayer({
         displayValues: true,
         displayOptions: {
